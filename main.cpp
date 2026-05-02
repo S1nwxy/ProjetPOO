b373b84661ca498119d1620a94712eafff7630ff
S1nwxy <johann.plasse@gmail.com>
2026-05-02 18:05:30 +0200

    main test script to test new InputHandling methods
    Everything works except parse which creates a vector of Command, but no
    CommandWithParam inside, needs to be fixed

--- main.cpp
+++ main.cpp
@@ -5,33 +5,57 @@
 #include "menu.h"
 #include "menuoption.h"
 #include "menuaction.h"
+#include "command.h"
 #include "InputHandling.h"
 
 using namespace std;
 
 int main(){
-    Input test1 = Input("fd 10 turn 20 ");
-    Input test2 = Input("fd 10 repeat 2 [turn 10] clear"); // fd 10 turn 10 turn 10 clear
-    Input test3 = Input("fd 10 repeat 2 [turn 10 repeat 2 [fd -5] turn -5] clear");
+    // /**** INFO: TEST SCRIPT
+    InputHandling test1 = InputHandling("fd 10 turn 20 ");
+    InputHandling test2 = InputHandling("fd 10 repeat 2 [turn 10] clear"); // fd 10 turn 10 turn 10 clear
+    InputHandling test3 = InputHandling("fd 10 repeat 2 [turn 10 repeat 2 [fd -5] turn -5] clear");
     // fd 10 (turn 10 fd -5 fd -5 turn -5) turn 10 fd -5 fd -5 turn -5 clear
-    std::cout << test1.deloop() << endl;
-    std::cout << test2.deloop() << endl;
-    std::cout << test3.deloop() << endl;
-    vector<string> test1vec = test1.vectorize(test1.deloop());
-    vector<string> test2vec = test2.vectorize(test2.deloop());
-    vector<string> test3vec = test3.vectorize(test3.deloop());
+    test1.deloop();
+    test2.deloop();
+    test3.deloop();
+    std::cout << test1.delooped() << endl;
+    std::cout << test2.delooped() << endl;
+    std::cout << test3.delooped() << endl;
+    test1.vectorize();
+    test2.vectorize();
+    test3.vectorize();
     cout << endl;
-    for(string it:test1vec){
-        cout << it << ' ';
+    int i = 0;
+    for(string it : test1.vectorized()){
+        cout << i << ": " << it << ' ';
+        i++;
+    }
+    i = 0;
+    cout << endl;
+    for(string it : test2.vectorized()){
+        cout << i << ": " << it << ' ';
+        i++;
+    }
+    i = 0;
+    cout << endl;
+    for(string it : test3.vectorized()){
+        cout << i << ": "<< it << ' ';
+        i++;
     }
     cout << endl;
-    for(string it:test2vec){
-        cout << it << ' ';
-    }
+
+    test1.parse();
+
     cout << endl;
-    for(string it:test3vec){
-        cout << it << ' ';
+    i = 0;
+    for(auto it : test1.result()){
+        cout << i << ": ";
+        it.display();
+        cout << ' ';
+        i++;
     }
-    cout << endl;
+    
+    // */
     return 0;
 }
